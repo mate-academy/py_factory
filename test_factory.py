f51545d57185a382cfafae064c899463054490d4
koreyo <KoretsValentyn@gmail.com>
2019-12-08 23:27:39 +0200

    first

--- test_factory.py
+++ test_factory.py
@@ -3,9 +3,10 @@ import factory
 
 def test_zoo():
     z = factory.Zoo('zoo.csv')
-    assert [animal.say() for animal in z.animals()] == ['Woof', 'Meow', 'Meow', 'Tweet']
+    print(z)
+    assert [animal.say() for animal in z.get_animal()] == ['Woof', 'Meow', 'Meow', 'Tweet']
 
 
 def test_weight():
     z = factory.Zoo('zoo.csv')
-    assert sum(animal.weight for animal in z.animals()) == 48.4
+    assert sum(animal.weight for animal in z.get_animal()) == 48.4

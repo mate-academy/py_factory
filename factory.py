1d14d3d68bbf59682f2b8575f2222c92bef40424
Platoniuks <platon@alterpost.org>
2019-12-09 07:49:21 +0200

    resolved

--- factory.py
+++ factory.py
@@ -30,61 +30,42 @@ class Zoo:
         return self._factory
 
 
-class Dog:
+class Animal:
+    """Common animal class"""
+    def __init__(self, name, weight):
+        self._name = name
+        self._weight = float(weight)
+        self._say = ''
+
+    def get_name(self):
+        """Get animal's name"""
+        return self._name
+
+    def say(self):
+        """Animal's voice"""
+        return self._say
+
+    def get_weight(self):
+        """Get animal's weight"""
+        return self._weight
+
+
+class Dog(Animal):
     """Class of dog"""
     def __init__(self, name, weight):
-        self._name = name
+        super().__init__(name, weight)
         self._say = "Woof"
-        self._weight = float(weight)
-
-    def get_name(self):
-        """Get name of a dog"""
-        return self._name
-
-    def say(self):
-        """What does a dog say?"""
-        return self._say
-
-    def get_weight(self):
-        """Get animal's weight"""
-        return self._weight
 
 
-class Cat:
+class Cat(Animal):
     """Class of cat"""
     def __init__(self, name, weight):
-        self._name = name
+        super().__init__(name, weight)
         self._say = "Meow"
-        self._weight = float(weight)
-
-    def get_name(self):
-        """Get name of a cat"""
-        return self._name
-
-    def say(self):
-        """What does a cat say?"""
-        return self._say
-
-    def get_weight(self):
-        """Get animal's weight"""
-        return self._weight
 
 
-class Bird:
+class Bird(Animal):
     """Class of bird"""
     def __init__(self, name, weight):
-        self._name = name
+        super().__init__(name, weight)
         self._say = "Tweet"
-        self._weight = float(weight)
-
-    def get_name(self):
-        """Get name of a bird"""
-        return self._name
-
-    def say(self):
-        """What does a bird say?"""
-        return self._say
-
-    def get_weight(self):
-        """Get animal's weight"""
-        return self._weight
